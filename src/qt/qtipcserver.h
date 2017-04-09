9380f5774b1b11c4aade7642a4a17b5f13510371
cannation <cryptotrader69@protonmail.com>
2017-04-08 18:11:45 -0700

    cannation init commit
    Inital commit of cannation project

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define cannation-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "cannationURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
